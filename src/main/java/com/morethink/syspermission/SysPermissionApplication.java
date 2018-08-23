2a7fa338c9ac5a27123742721bb4f74d6c8c4934
nowayingswang <nowayings@gmail.com>
2018-08-23 22:34:47 +0800

    Init project code

---
+++ src/main/java/com/morethink/syspermission/SysPermissionApplication.java
@@ -0,0 +1,12 @@
+package com.morethink.syspermission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysPermissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysPermissionApplication.class, args);
+	}
+}
