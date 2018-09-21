13d5a56cf383e5105a33b71a4ea86905f9b3209a
wangpf <wangpf@csdn.net>
2018-09-21 19:20:16 +0800

    testcontroller

--- src/main/java/com/morethink/syspermission/controller/TestController.java
+++ src/main/java/com/morethink/syspermission/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.morethink.syspermission.controller;
 
+import com.morethink.syspermission.common.ApplicationContextHelper;
 import com.morethink.syspermission.common.JsonData;
+import com.morethink.syspermission.dao.user.SysUserMapper;
+import com.morethink.syspermission.entity.user.SysUser;
 import com.morethink.syspermission.param.TestVo;
 import com.morethink.syspermission.util.BeanValidator;
+import com.morethink.syspermission.util.JsonMapper;
 import org.apache.commons.collections.MapUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import sun.rmi.runtime.Log;
 
 import java.util.Map;
 
@@ -34,6 +39,9 @@ public class TestController {
 
         }
 
+        SysUserMapper sysUserMapper = ApplicationContextHelper.getBean(SysUserMapper.class);
+        SysUser sysUser = sysUserMapper.selectByPrimaryKey(1);
+        System.out.println(">>>>>>>>> " + JsonMapper.object2String(sysUser));
         return JsonData.success("test validate");
     }
 
